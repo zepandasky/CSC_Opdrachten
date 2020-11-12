ef1ecf6127b02febdd4d640121fc85b944867d23
zepandasky <zepholleman2012@hotmail.nl>
2020-11-12 15:19:33 +0100

    empty main branch

--- README.md
+++
@@ -1,7 +0,0 @@
-# CSC_Opdrachten
-De opdrachten per week voor Configurable System on a Chip
-
-branches zijn de code per week,
-dus branch : week1_practicum1 = is week 1 en dan woensdag practicum en practicum2 verwijst dan naar de vrijdag van de eerste week
-
-GROETJES
--- nios_system/nios_system_inst.v
+++
@@ -1,9 +0,0 @@
-	nios_system u0 (
-		.clk_clk         (<connected-to-clk_clk>),         //      clk.clk
-		.hex0_3_export   (<connected-to-hex0_3_export>),   //   hex0_3.export
-		.hex4_5_export   (<connected-to-hex4_5_export>),   //   hex4_5.export
-		.leds_export     (<connected-to-leds_export>),     //     leds.export
-		.reset_reset_n   (<connected-to-reset_reset_n>),   //    reset.reset_n
-		.switches_export (<connected-to-switches_export>)  // switches.export
-	);
-
